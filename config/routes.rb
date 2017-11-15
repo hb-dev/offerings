8da4c02c5499757481b6996c49d1e5ebac0c25a4
Henrik Boettcher <enovateweb@gmail.com>
2017-11-15 09:03:25 +0100

    remove obsolete stop files, model only

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :offerings
-  resources :stops
   resources :routes
   resources :ports
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
